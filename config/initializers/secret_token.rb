2ea1c44aa57d10cb5cf1a0e134aad8a0650686b8
Heath Ritchie <heath@heathritchie.com>
2012-06-23 18:08:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RidiculouslyLongLinkShortener::Application.config.secret_token = 'df0fe6eb75de090196c338911b31657f6775d166bb2d1f10ba8b49d42c798d22287672cf71622ba8f4625771d4556d148c640cc0d5544616933c733c142a1322'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RidiculouslyLongLinkShortener::Application.config.session_store :cookie_store, key: '_RidiculouslyLongLinkShortener_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RidiculouslyLongLinkShortener::Application.config.session_store :active_record_store
