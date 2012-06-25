f152ed2e97f16bb6bb330c2f15bcdcd7a5b00ce5
Heath Ritchie <heath@heathritchie.com>
2012-06-25 08:30:07 -0400

    Add 'links' and new, create, and index views

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,17 @@
 class LinksController < ApplicationController
+  def index
+    @links = Link.all
+  end
+  def new 
+    @link = Link.new
+  end
+
+  def create
+    link = Link.new(params[:link])
+    if link.save
+      redirect_to links_path
+    else
+      render 'new'
+    end
+  end
 end
