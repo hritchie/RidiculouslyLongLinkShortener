d5740ceac8c1c99276a39c7f682120cab9553c66
Heath Ritchie <heath@heathritchie.com>
2012-06-25 19:00:26 -0400

    Add destroy capability for links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,6 +2,7 @@ class LinksController < ApplicationController
   def index
     @links = Link.all
   end
+
   def new 
     @link = Link.new
   end
@@ -13,5 +14,11 @@ class LinksController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def destroy
+    link = Link.find_by_id(params[:id])
+    link.delete
+    redirect_to root_path
   end
 end
